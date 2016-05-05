4f4eddf7b5f81c85c1c291d679ee88b17edb5dca
diitaz93 <juantrix93@hotmail.com>
2016-05-05 12:47:33 -0500

    imprime esfericas y cartesianas

--- init.c
+++ init.c
@@ -5,6 +5,8 @@
 #define PI 3.14159265358979323846
 
 void random_polar(int N, double r, double P[]);
+void polar2cartesian(int N, double P[], double X[]);
+void print3columns(int N, double vec[]);
 
 int main (int argc, char **argv)
 {
@@ -21,23 +23,66 @@ int main (int argc, char **argv)
   double r=0.6205*s; // Radius of sphere
 
 
-  /* CREATE RANDOM VARIABLES*/
-
+  /* Create polar coordinate data*/
   random_polar(N,r,P);
-  // Prints 3 columns
+  /*Transform to cartesian coordinates*/
+  polar2cartesian(N,P,X);
+  /* Print Polar coordinates*/
+  print3columns(N,P);
+  printf("\n");
+  /* Print cartesian coordinates */
+  print3columns(N,X);
+}
+
+void print3columns(int N, double vec[])
+/*
+Prints a 3N length array of 3D coordinates of the form
+[q1 q2 q3] in three columns using printf
+ */
+{
   int i;
   for (i=0;i<N;i++)
     {
-      printf("%.3f \t %.3f \t %.3f \n",P[i],P[i+N],P[i+2*N]);
+      printf("%6.3f\t%6.3f\t%6.3f\n",vec[i],vec[i+N],vec[i+2*N]);
     }
 }
-/* Create random points in spherical coordinates*/
+
+void polar2cartesian(int N, double P[], double X[])
+/*
+ Transforms a 3N lenght array of shperical coordinates
+of the form [R Th Phi] to a cartesian coordinate array
+of the form [X Y Z].
+*/ 
+{
+  int i;
+  double R;
+  double Th;
+  double Phi;
+  for (i=0;i<N;i++)
+    {
+      R=P[i];
+      Th=P[i+N];
+      Phi=P[i+2*N];
+      
+      X[i]=R*cos(Th)*sin(Phi);
+      X[i+N]=R*sin(Th)*sin(Phi);
+      X[i+2*N]=R*cos(Phi);
+    }
+}
+
 void random_polar(int N, double r, double P[])
+/*
+ Create random points in spherical coordinates:
+The array P is a linear array of length 3N
+in which the first N entries are R coordinates,
+the next N are polar angle coordinates (Theta),
+and the last N entries are azimuthal angles (Phi)
+*/
 {
   int i;
   srand48(time(NULL)); //random seed
   double max=r;
-  double min=-r;
+  double min=0;
   int cont=0;
   for (i=0;i<3*N;i++)
     {
@@ -47,13 +92,11 @@ void random_polar(int N, double r, double P[])
 	  if(cont==0)
 	    {
 	      max=2*PI;
-	      min=0;
 	      cont++;
 	    }
 	  else if (cont==1)
 	    {
 	      max=PI;
-	      min=0;
 	    }
 	  srand48(time(NULL));
 	}
@@ -63,3 +106,5 @@ void random_polar(int N, double r, double P[])
 	}
     }
 }
+
+
